0056d837f01a2ec214ac1b8a7b7c88691fe36eb4
Josh Varty <joshvarty@gmail.com>
2020-10-13 17:45:04 -0400

    Implement FP.4

--- Project4-CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.cpp
+++ Project4-CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.cpp
@@ -18,6 +18,9 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lock(_mutex);
+    _queue.emplace_back(std::move(msg));
+    MessageQueue::_condition.notify_one();
 }
 
 /* Implementation of class "TrafficLight" */
@@ -75,6 +78,7 @@ void TrafficLight::cycleThroughPhases()
             }
 
             //TODO: Add phase to message queue?
+            _messageQueue.send(std::move(phase));
 
             time_until_change = distr(gen);
             last_time = std::chrono::steady_clock::now();
--- Project4-CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.h
+++ Project4-CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.h
@@ -64,7 +64,7 @@ private:
     TrafficLightPhase _currentPhase;
     std::condition_variable _condition;
     std::mutex _mutex;
-
+    MessageQueue<TrafficLightPhase> _messageQueue;
 };
 
 #endif
