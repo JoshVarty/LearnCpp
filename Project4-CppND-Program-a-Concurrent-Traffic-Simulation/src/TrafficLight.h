c65cb4deaec63c2466930fb6f6a24a39a039f5d1
Josh Varty <joshvarty@gmail.com>
2020-10-12 23:12:51 -0400

    Enable TrafficLight

--- Project4-CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.h
+++ Project4-CppND-Program-a-Concurrent-Traffic-Simulation/src/TrafficLight.h
@@ -43,7 +43,7 @@ public:
     TrafficLight();
 
     // getters / setters
-    TrafficLightPhase getCurrentPhase() { return _currentPhase; }
+    TrafficLightPhase getCurrentPhase();
 
     // typical behaviour methods
     void waitForGreen();
